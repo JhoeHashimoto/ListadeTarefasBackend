0bcb839d327c866303e6a5bf6a64d184eb79c5be
Jhoe Hashimoto <jhoeyoshiohashimoto@gmail.com>
2024-08-14 23:02:40 -0300

    Introdução TSX

---
+++ TypeScript/dados.js
@@ -0,0 +1,5 @@
+var nome = "Jhoe";
+var idade = 25;
+var casado = false;
+console.log("Nome:", $, { nome: nome });
+console.log('Idade:', $, { idade: idade });
---
+++ TypeScript/dados.ts
@@ -0,0 +1,8 @@
+let nome: string = "Jhoe";
+let idade: number = 25;
+let casado: boolean = false;
+
+
+console.log(`Nome: ${nome}`);
+console.log(`Idade: ${idade}`);
+console.log(`Casado: ${casado}`);
