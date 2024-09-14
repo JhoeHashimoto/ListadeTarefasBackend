1611fc201df3e8ab0997c1388af13eb0518f7c07
Jhoe Hashimoto <147004858+JhoeHashimoto@users.noreply.github.com>
2024-09-13 22:37:39 -0300

    Delete TypeScript directory

--- TypeScript/dados.js
+++
@@ -1,5 +0,0 @@
-var nome = "Jhoe";
-var idade = 25;
-var casado = false;
-console.log("Nome:", $, { nome: nome });
-console.log('Idade:', $, { idade: idade });
--- TypeScript/dados.ts
+++
@@ -1,8 +0,0 @@
-let nome: string = "Jhoe";
-let idade: number = 25;
-let casado: boolean = false;
-
-
-console.log(`Nome: ${nome}`);
-console.log(`Idade: ${idade}`);
-console.log(`Casado: ${casado}`);
